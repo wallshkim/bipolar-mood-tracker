6ea42fac60d40338d800ec04c00c4ee4f4f019c9
Heather Walls <wallshkim@gmail.com>
2019-11-19 15:54:56 -0600

    Installs momentjs

--- src/components/MoodLog/MoodLog.js
+++ src/components/MoodLog/MoodLog.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 
 class MoodLog extends Component {
 
+
     render() {
+
+        let weekday = moment().format('dddd');
+        console.log(weekday);
+        let todaysDate = moment().format('MMMM Do, YYYY');
+        console.log(todaysDate);
+        let yesterday = moment().add(-1, 'days')
+        console.log(yesterday);
+        
         return (
             <div>
-                <h1>Today</h1>
-                
+                <h3>{weekday}</h3>
+                <h6>{todaysDate}</h6>
+
+
             </div>
         );
     }
