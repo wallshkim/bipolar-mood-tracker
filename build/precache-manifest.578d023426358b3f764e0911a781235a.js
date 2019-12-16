153fb4f25f6408383bb32875f3b516bb3bb2aee2
Heather Walls <wallshkim@gmail.com>
2019-12-15 22:08:08 -0600

    Adds build folder

---
+++ build/precache-manifest.578d023426358b3f764e0911a781235a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e3a4019cb009f8accf6d7f0b7dd3cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32322bc0d0ce5efa448",
+    "url": "/static/css/main.27a03acb.chunk.css"
+  },
+  {
+    "revision": "d3a2685999be51c2f850",
+    "url": "/static/js/2.d563484d.chunk.js"
+  },
+  {
+    "revision": "b32322bc0d0ce5efa448",
+    "url": "/static/js/main.caafc176.chunk.js"
+  },
+  {
+    "revision": "677b53b4f990945ee06a",
+    "url": "/static/js/runtime-main.1a87a22e.js"
+  },
+  {
+    "revision": "597c1424b5c1bbf9041a713ada63a662",
+    "url": "/static/media/compassblackfont.597c1424.png"
+  }
+]);
